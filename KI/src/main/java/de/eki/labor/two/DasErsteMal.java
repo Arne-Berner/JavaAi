414c63014242529f8a81ee15754c2d017a94bfd3
Arne Berner <abcinematics@gmail.com>
2022-11-07 18:07:32 +0100

    first commit

---
+++ KI/src/main/java/de/eki/labor/two/DasErsteMal.java
@@ -0,0 +1,9 @@
+package de.eki.labor.two;
+
+import de.fhkiel.ki.cathedral.gui.CathedralGUI;
+
+public class DasErsteMal {
+    public static void main(String[] args) {
+        CathedralGUI.start();
+    }
+}
